3b8ab25eedda235a256bf99ec6ce29f40f0e4c66
Victor Semionov <vsemionov@gmail.com>
2025-05-15 18:26:09 +0300

    read-only database accounts

--- docker/ch-init-user-db.sh
+++ docker/ch-init-user-db.sh
@@ -3,8 +3,10 @@ set -e
 
 clickhouse client <<-EOSQL
     CREATE USER fleet IDENTIFIED BY '$FLEET_WAREHOUSE_PASSWORD';
+    CREATE USER reader IDENTIFIED BY '$READER_WAREHOUSE_PASSWORD';
 
     CREATE DATABASE fleet;
 
     GRANT ALL ON fleet.* TO fleet;
+    GRANT SELECT ON fleet.* TO reader;
 EOSQL
