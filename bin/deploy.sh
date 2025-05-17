961508ef0d8d44173889bc32d56a5ac0d65cbbb4
Victor Semionov <vsemionov@gmail.com>
2025-05-17 12:35:13 +0300

    set up grafana with redis as a cache

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -35,4 +35,12 @@ echo "Enabling Airflow DAGs"
 docker compose exec airflow bash -c "airflow dags unpause process_states"
 docker compose exec airflow bash -c "airflow dags unpause collect_states"
 
+echo "Adding Grafana datasources"
+docker compose exec -T grafana bash <<EOL
+curl -f -sS -u "\$GF_ADMIN_USER:\$GF_ADMIN_PASSWORD" "http://localhost:3000/api/datasources/name/postgres" >/dev/null || curl -f -sS -X POST -u "\$GF_ADMIN_USER:\$GF_ADMIN_PASSWORD" -H "Content-Type: application/json" --data '{"id":1,"uid":"dem4xij9821hcc","orgId":1,"name":"postgres","type":"grafana-postgresql-datasource","typeName":"PostgreSQL","typeLogoUrl":"public/app/plugins/datasource/grafana-postgresql-datasource/img/postgresql_logo.svg","access":"proxy","url":"postgres","user":"reader","secureJsonData":{"password":"'"\$READER_DATABASE_PASSWORD"'"},"database":"","basicAuth":false,"isDefault":false,"jsonData":{"connMaxLifetime":14400,"database":"fleet","maxIdleConns":20,"maxIdleConnsAuto":true,"maxOpenConns":20,"postgresVersion":1500,"sslmode":"disable"},"readOnly":false}' "http://localhost:3000/api/datasources" >/dev/null
+EOL
+docker compose exec -T grafana bash <<EOL
+curl -f -sS -u "\$GF_ADMIN_USER:\$GF_ADMIN_PASSWORD" "http://localhost:3000/api/datasources/name/clickhouse" >/dev/null || curl -f -sS -X POST -u "\$GF_ADMIN_USER:\$GF_ADMIN_PASSWORD" -H "Content-Type: application/json" --data '{"id":2,"uid":"dem4xxg0tkao0a","orgId":1,"name":"clickhouse","type":"grafana-clickhouse-datasource","typeName":"ClickHouse","typeLogoUrl":"public/plugins/grafana-clickhouse-datasource/img/logo.svg","access":"proxy","url":"","user":"","database":"","basicAuth":false,"isDefault":true,"jsonData":{"defaultDatabase":"fleet","host":"clickhouse","logs":{"contextColumns":[],"defaultTable":"otel_logs","otelVersion":"latest","selectContextColumns":true},"port":9000,"protocol":"native","traces":{"defaultTable":"otel_traces","durationUnit":"nanoseconds","otelVersion":"latest"},"username":"reader","version":"4.8.2"},"secureJsonData":{"password":"'"\$READER_DATABASE_PASSWORD"'"},"readOnly":false}' "http://localhost:3000/api/datasources" >/dev/null
+EOL
+
 echo "You can now delete data/aircraft.parquet (unless you plan to run this script again)"
