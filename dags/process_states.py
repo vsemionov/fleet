a20b06f05a6411d5c12bc3c417758c6bda668859
Victor Semionov <vsemionov@gmail.com>
2025-06-02 06:49:45 +0300

    plot regions

--- dags/process_states.py
+++ dags/process_states.py
@@ -3,7 +3,7 @@ import os
 import time
 from datetime import timedelta, timezone
 from pathlib import Path
-from typing import Callable
+from typing import Optional, Callable
 import logging
 
 from airflow.sdk import dag, task, get_current_context
@@ -23,7 +23,12 @@ import matplotlib.pyplot as plt
 from daglib import config
 
 
+Rect = tuple[float, float, float, float]
+Size = tuple[int, int]
+
+
 logger = logging.getLogger('airflow.task')
+
 
 @dag(
     description='Process states',
@@ -45,6 +50,7 @@ def process_states():
             .master(f'spark://{spark_conn.host}:{spark_conn.port or config.SPARK_DEFAULT_PORT}') \
             .config('spark.jars', f'{config.CLICKHOUSE_JDBC_JAR}') \
             .getOrCreate()
+
 
     def load_data(spark: SparkSession) -> sql.DataFrame:
         context = get_current_context()
@@ -73,8 +79,8 @@ def process_states():
             .option('upperBound', str(data_interval_end)) \
             .load()
 
-    def create_projection() -> tuple[cp.crs.Projection, Callable]:
-        projection = cp.crs.Robinson()
+
+    def create_projection(projection: cp.crs.Projection) -> Callable:
         geodetic = cp.crs.Geodetic()
 
         @F.pandas_udf('x float, y float')
@@ -82,12 +88,20 @@ def process_states():
             x, y = projection.transform_points(geodetic, lon, lat).T[:2]
             return pd.DataFrame({'x': x, 'y': y})
 
-        return projection, project_coords
+        return project_coords
 
-    def image_coords(df: sql.DataFrame, projection: cp.crs.Projection) -> sql.DataFrame:
-        w, h = config.PLOT_AGG_RESOLUTION
-        x_start, x_end = projection.x_limits
-        y_start, y_end = projection.y_limits
+
+    def filter_crop(df: sql.DataFrame, window: Rect) -> sql.DataFrame:
+        x_start, x_end, y_start, y_end = window
+        return df.filter(
+            (F.col('coords.x') >= x_start) & (F.col('coords.x') < x_end) &
+            (F.col('coords.y') >= y_start) & (F.col('coords.y') < y_end)
+        )
+
+
+    def image_coords(df: sql.DataFrame, window: Rect, resolution: Size) -> sql.DataFrame:
+        x_start, x_end, y_start, y_end = window
+        w, h = resolution
         wx = w / (x_end - x_start)
         wy = h / (y_end - y_start)
         return df.select(
@@ -96,61 +110,142 @@ def process_states():
             (F.floor((F.col('coords.y') - y_start) * wy) % h).alias('y'),
         )
 
-    def to_array(df: sql.DataFrame) -> xr.DataArray:
+
+    def to_array(df: sql.DataFrame, resolution: Size) -> xr.DataArray:
         df = df.toPandas()
         yx = df[['y', 'x']].to_numpy()
         v = df['v'].to_numpy()
 
-        agg = np.zeros(config.PLOT_AGG_RESOLUTION[::-1], dtype=np.uint64)
+        agg = np.zeros(resolution[::-1], dtype=np.uint64)
         agg[yx[:, 0], yx[:, 1]] = v
         return xr.DataArray(agg)
 
-    def get_output_path() -> Path:
+
+    def get_output_path(slug: str) -> Path:
         output_dir = Path(config.PLOT_OUTPUT_DIR)
         output_dir.mkdir(parents=True, exist_ok=True)
-        plot_id = get_current_context()['data_interval_start'].astimezone(timezone.utc).strftime('%Y-%m-%d')
-        return output_dir / f'traffic-{plot_id}.png'
+        plot_group = get_current_context()['data_interval_start'].astimezone(timezone.utc).strftime('%Y-%m-%d')
+        return output_dir / f'traffic-{plot_group}-{slug}.png'
 
-    def rasterize(agg: xr.DataArray, projection: cp.crs.Projection, output_path: Path) -> None:
+
+    def rasterize(
+            agg: xr.DataArray,
+            projection: cp.crs.Projection,
+            window: Rect,
+            figsize: float,
+            fontsize: float,
+            title: str,
+            output_path: Path
+    ) -> None:
         img = ds.tf.shade(agg, cmap=cc.fire)
         img = img.to_pil()
 
         plt.style.use('dark_background')
-        plt.figure(figsize=(config.PLOT_FIG_SIZE, config.PLOT_FIG_SIZE))
+        plt.figure(figsize=(figsize, figsize))
 
         try:
             ax = plt.axes(projection=projection)
             ax.coastlines(color='grey')
             ax.add_feature(cp.feature.LAND, facecolor='#202020')
             ax.add_feature(cp.feature.BORDERS, edgecolor='grey', linewidth=0.5)
-            ax.imshow(img, extent=(projection.x_limits + projection.y_limits), transform=projection, zorder=2)
-            plt.title('Traffic Density', fontsize=config.PLOT_TITLE_SIZE)
-            plt.savefig(output_path, bbox_inches='tight', pad_inches=config.PLOT_PAD_SIZE)
+            ax.imshow(img, extent=window, transform=projection, zorder=2)
+            plt.title(title, fontsize=fontsize)
+            plt.savefig(output_path, bbox_inches='tight', pad_inches=(figsize / 100))
 
         finally:
             plt.close()
 
 
+    def plot_traffic(
+            states: sql.DataFrame,
+            projection: cp.crs.Projection,
+            region: Optional[Rect],
+            resolution: Size,
+            figsize: float,
+            fontsize: float,
+            title: str,
+            slug: str
+    ) -> None:
+        if region is not None:
+            w, e, s, n = region
+            geodetic = cp.crs.Geodetic()
+            x_start, y_start = projection.transform_point(w, s, geodetic)
+            x_end, y_end = projection.transform_point(e, n, geodetic)
+        else:
+            x_start, x_end = projection.x_limits
+            y_start, y_end = projection.y_limits
+        window = x_start, x_end, y_start, y_end
+
+        project_coords = create_projection(projection)
+
+        t0 = time.time()
+        df = states.select(project_coords('longitude', 'latitude').alias('coords'))
+        if region is not None:
+            df = filter_crop(df, window)
+        df = image_coords(df, window, resolution)
+        df = df.select('x', 'y').groupBy('x', 'y').agg(F.count('*').alias('v'))
+        agg = to_array(df, resolution)
+        logger.info(f'Processing took {time.time() - t0:.3f} s')
+
+        output_path = get_output_path(slug)
+        rasterize(agg, projection, window, figsize, fontsize, title, output_path)
+        logger.info(f'Saved to: {output_path}')
+
+
     @task()
     def process():
         with spark_session() as spark:
-            projection, project_coords = create_projection()
-            logger.info(f'Setup complete')
-
             t0 = time.time()
-            df = load_data(spark)
-            logger.info(f'Loaded {df.count()} records in {time.time() - t0:.3f} s')
+            states = load_data(spark)
+            logger.info(f'Loaded {states.count()} records in {time.time() - t0:.3f} s')
 
-            t0 = time.time()
-            df = df.select(project_coords('longitude', 'latitude').alias('coords'))
-            df = image_coords(df, projection)
-            df = df.select('x', 'y').groupBy('x', 'y').agg(F.count('*').alias('v'))
-            agg = to_array(df)
-            logger.info(f'Processing took {time.time() - t0:.3f} s')
+            logger.info('Plotting traffic density - global')
+            plot_traffic(
+                states,
+                cp.crs.Robinson(),
+                None,
+                (2048, 1024),
+                25,
+                20,
+                'Traffic Density - Global',
+                'global'
+            )
 
-            output_path = get_output_path()
-            rasterize(agg, projection, output_path)
-            logger.info(f'Saved to: {output_path}')
+            logger.info('Plotting traffic density - Europe')
+            plot_traffic(
+                states,
+                cp.crs.Mercator(),
+                (-18, 50, 28, 72),
+                (1024, 512),
+                15,
+                16,
+                'Traffic Density - Europe',
+                'europe'
+            )
+
+            logger.info('Plotting traffic density - North America')
+            plot_traffic(
+                states,
+                cp.crs.Mercator(),
+                (-180, -50, 0, 75),
+                (1024, 512),
+                15,
+                16,
+                'Traffic Density - North America',
+                'north_america'
+            )
+
+            logger.info('Plotting traffic density - United States')
+            plot_traffic(
+                states,
+                cp.crs.Mercator(),
+                (-130, -55, 15, 55),
+                (1024, 512),
+                15,
+                12,
+                'Traffic Density - United States',
+                'united_states'
+            )
 
 
     process()
